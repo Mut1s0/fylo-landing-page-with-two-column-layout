2224ccaff474997431b37b65f0cad7884e86f0f5
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-29 12:26:49 +0300

    Added navigation section

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,33 @@
+import { navigation } from "../constants";
+import { logo } from "../assets";
 
 function Navigation() {
 
     return(
 
-        <h1>Navigation</h1>
+        <section className="max-w-[90%] mx-auto font-raleway">
+
+            <div className="flex justify-between items-center py-[50px]">
+
+                <img src={logo} alt="logo" />
+
+                <div className="flex gap-[70px]">
+
+                    {navigation.map((nav) => (
+
+                        <ul key={nav.id}>
+
+                            <li className="font-regularRaleway text-[1.3rem]">{nav.text}</li>
+
+                        </ul>
+
+                    ))}
+
+                </div>
+
+            </div>
+
+        </section>
         
     )
 }
